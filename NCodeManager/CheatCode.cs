2d6cca8f3c2cda90cdae30abbe8ea4deb6943ab4
Natalie <nat8badf00d@gmail.com>
2021-10-03 23:27:47 -0400

    Organize

--- NCodeManager/CheatCode.cs
+++ NCodeManager/CheatCode.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Globalization;
 using System.Windows.Forms;
+
+using static NCodeManager.Helpers;
 
 namespace NCodeManager
 {
@@ -39,26 +37,6 @@ namespace NCodeManager
                 instructionsString += string.Format("{0:X8} {1:X8}\r\n", instruction.Item1, instruction.Item2);
 
             return instructionsString;
-        }
-
-        private static bool HexStringToUInt(string hex, out uint val)
-        {
-            return uint.TryParse(hex, NumberStyles.HexNumber, null, out val);
-        }
-
-        private static uint ReadUIntSwappedEndianness(BinaryReader binaryReader)
-        {
-            uint val = binaryReader.ReadUInt32();
-            val = (val >> 16) | (val << 16);
-            val = ((val & 0xFF00FF00) >> 8) | ((val & 0x00FF00FF) << 8);
-            return val;
-        }
-
-        private static void WriteUIntSwappedEndianness(BinaryWriter binaryWriter, uint val)
-        {
-            val = (val >> 16) | (val << 16);
-            val = ((val & 0xFF00FF00) >> 8) | ((val & 0x00FF00FF) << 8);
-            binaryWriter.Write(val);
         }
 
         public void Parse(string instructionsString)
---
+++ NCodeManager/Helpers.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.IO;
+
+namespace NCodeManager
+{
+    static class Helpers
+    {
+        public static bool HexStringToUInt(string hex, out uint val)
+        {
+            return uint.TryParse(hex, NumberStyles.HexNumber, null, out val);
+        }
+
+        public static uint ReadUIntSwappedEndianness(BinaryReader binaryReader)
+        {
+            uint val = binaryReader.ReadUInt32();
+            val = (val >> 16) | (val << 16);
+            val = ((val & 0xFF00FF00) >> 8) | ((val & 0x00FF00FF) << 8);
+            return val;
+        }
+
+        public static void WriteUIntSwappedEndianness(BinaryWriter binaryWriter, uint val)
+        {
+            val = (val >> 16) | (val << 16);
+            val = ((val & 0xFF00FF00) >> 8) | ((val & 0x00FF00FF) << 8);
+            binaryWriter.Write(val);
+        }
+    }
+}
